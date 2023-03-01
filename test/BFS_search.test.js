53530acb71254545e3e6a39108baa5e6a509e6db
Xinrui Jiang <jqjxxjxr@126.com>
2023-03-01 17:22:46 -0500

    add more tests

--- test/BFS_search.test.js
+++ test/BFS_search.test.js
@@ -1,7 +1,7 @@
 import { BFS_search } from '../js/lib/BFS_search'
 import { Graph } from '../js/lib/graph'
 
-test('BFS on graph with 2 node, 1 edge', () => {
+test('BFS on graph with 2 nodes, 1 edge', () => {
     let g = new Graph();
     let nodeA = g.addNode();
     let nodeB = g.addNode();
@@ -14,4 +14,23 @@ test('BFS on graph with 2 node, 1 edge', () => {
     expect(path.length).toBe(2);
     expect(path[0]).toBe(nodeB);
     expect(path[1]).toBe(nodeA);
+
+    //test getProcess function
+    let queue=[],
+        visited=[];
+    let visited0=[],
+        visited1=[];
+    visited0.push(0);
+    visited1.push(1);
+    visited.push(visited0,visited1);
+    queue.push(0,1);
+    expect(search.getProcess()).toMatchObject({"OneDArray":queue,"TwoDArray":visited});
 });
+
+test('BFS search grach with 2 nodes, 0 edge',()=>{
+    let g = new Graph();
+    let nodeA = g.addNode();
+    let nodeB = g.addNode();
+    let search = new BFS_search(g, nodeA);
+    expect(search.pathTo(nodeB)).toBe(null);
+})
--- test/linkedList.test.js
+++ test/linkedList.test.js
@@ -6,14 +6,22 @@ test('crate a Node object',()=>{
     expect(node.next).toBe(null);
 });
 
-test('linkedlist',()=>{
+test('linkedlist1',()=>{
     var linkedList=new LinkedList();
     expect(linkedList.getSize()).toBe(0);
     expect(linkedList.getAtIndex(0)).toBe(null);
     expect(linkedList.isEmpty()).toBe(true);
     linkedList.addAtEnd(2);
     linkedList.addAtStart(1);
-    expect(linkedList.getSize()).toBe(2);
+    linkedList.addAtEnd(3);
+    expect(linkedList.getSize()).toBe(3);
     expect(linkedList.getAtIndex(0)).toBe(1);
     expect(linkedList.getAtIndex(1)).toBe(2);
+    expect(linkedList.getAtIndex(2)).toBe(3);
+});
+
+test('linkedlist2',()=>{
+    var linkedList=new LinkedList();
+    linkedList.addAtStart(0);
+    expect(linkedList.getAtIndex(0)).toBe(0);
 });
