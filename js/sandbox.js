b75db0c7f79128c3514c37860700897df81337ee
Chunlin Min <c3min@uwaterloo.ca>
2023-03-12 22:07:52 -0400

    insertion sort demo in sandbox

--- js/sandbox.js
+++ js/sandbox.js
@@ -2,6 +2,7 @@
 import * as d3 from './thirdParty/d3.js';
 import {redirectConsoleOutput} from './lib/Logger.js'
 import {ArrayVisualizer} from './lib/DataVisualizer.js'
+import {InsertionSort} from './lib/SortingAlgorithms.js'
 
 // setup svg
 var width = 800,
@@ -21,25 +22,29 @@ redirectConsoleOutput(textArea)
 
 
 let tick = 0;
-let interval = 200; 
+let interval = 500; 
 // IMPORTANT: need to sync transition speed with interval, otherwise you transition may occur way too slow/fast with respect to change
 const g = new ArrayVisualizer(svg, interval)
 g.setLeftLabel("left label")
 g.setRightLabel("right label")
 g.setTitle("title")
 
-for (let i =0 ; i < 5; ++i)
-{
-    let data = {id : i, text: i}
-    setTimeout(function(){g.insertLeft(data)}, tick)
-    tick+=interval;
-}
-setTimeout(function(){g.insertRight({id:5, text: "hi"})}, tick)
+setTimeout(function(){g.insertLeft({id:0, text:1, value:1 })}, tick)
 tick+=interval;
-setTimeout(function(){g.removeLeft()}, tick)
+setTimeout(function(){g.insertLeft({id:1, text:5, value:5 })}, tick)
 tick+=interval;
-setTimeout(function(){g.remove(1)}, tick)
+setTimeout(function(){g.insertLeft({id:2, text:4, value:4 })}, tick)
 tick+=interval;
-setTimeout(function(){g.removeRight()}, tick)
+setTimeout(function(){g.insertLeft({id:3, text:0, value:0 })}, tick)
 tick+=interval;
-setTimeout(function(){g.insertBefore({id:6, text: "new"}, 0)}, tick)
+setTimeout(function(){g.insertLeft({id:4, text:2, value:2 })}, tick)
+tick+=interval;
+
+setTimeout(function(){
+    let sort = new InsertionSort(g)
+    for (let i = 0 ; i < 6; ++i)
+    {
+        setTimeout(function(){sort.step()}, tick)
+        tick+=interval;
+    }
+}, tick)
