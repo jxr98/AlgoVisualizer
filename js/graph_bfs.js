d8c3b3e2b9c798f8f36097de1ef2af4bdfca58a4
Xinrui Jiang <jqjxxjxr@126.com>
2023-03-19 15:37:54 -0400

    bfs

--- js/graph_bfs.js
+++ js/graph_bfs.js
@@ -4,24 +4,13 @@ import * as d3 from "./thirdParty/d3.js";
 import {ForceSimulationGraph} from "./lib/svg_graph.js";
 import {redirectConsoleOutput} from './lib/Logger.js'
 
-// TODO: perhaps its better if DOM elements are present in HTML and we just get handles to them
-// as opposed to creating svg, textarea, etc. DOM on the fly.
-
 //set up a svg
-const width = 1200;
-const height = 600;
-var svg = d3.select('body')
-    .append('svg')
-    .attr('width', width)
-    .attr('height', height);
+var svg = d3.select("#graphSvg")
 
 // setup log panel
 var visualizationDiv=d3.select("body").append("div");
 visualizationDiv.attr("class","col-12");
-let textArea = visualizationDiv.append("textarea")
-textArea.attr("readonly", true)
-.attr("cols", 30)
- .style("height", "100px")
+var textArea=d3.select("#logPanel");
 
 // console.log now mirrors to text area
 redirectConsoleOutput(textArea);
