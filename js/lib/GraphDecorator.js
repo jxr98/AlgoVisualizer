1d73749bca4f1aaa59b91e8bea16c266e2850798
Chunlin Min <c3min@uwaterloo.ca>
2023-04-05 17:50:54 -0400

    ignore graphDecorator from coverage

--- js/lib/GraphDecorator.js
+++ js/lib/GraphDecorator.js
@@ -7,12 +7,15 @@ export class GraphDecorator extends Graph{
         super();
         this.graph=graph;
     }
+    /* istanbul ignore next */
     addNode(x = 0, y = 0) {
         this.graph.addNode(x = 0, y = 0);
     }
+    /* istanbul ignore next */
     addEdge(v,w){
         this.graph.addEdge(v,w);
     }
+    /* istanbul ignore next */
     checkConnection(v,w){
         this.graph.checkConnection(v,w);
     }
