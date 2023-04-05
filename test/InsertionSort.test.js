df6e7e3be5f398e58489b0e011776e93f091a6b0
Chunlin Min <c3min@uwaterloo.ca>
2023-04-04 20:54:49 -0400

    test for graph template and sort template

--- test/InsertionSort.test.js
+++ test/InsertionSort.test.js
@@ -100,40 +100,5 @@ test('detailed step',()=>{
     // make sure no error
     expect(consoleSpy).toHaveBeenCalledTimes(0);
     consoleSpy.mockRestore();
-})
-
-
-//test arrayVis2Str
-test("should return an empty string if arrayVis is null", () => {
-    const result = arrayVis2Str(null);
-    expect(result).toEqual("");
-  });
-  
-  test("should convert an arrayVis object to a comma-separated string", () => {
-    const arrayVis = {
-      size: () => 3,
-      get: (index) => ({ value: `item${index}` }),
-    };
-    const result = arrayVis2Str(arrayVis);
-    expect(result).toEqual("item0,item1,item2");
-  });
-  
-  test("should convert an empty arrayVis object to an empty string", () => {
-    const arrayVis = {
-      size: () => 0,
-      get: (index) => null,
-    };
-    const result = arrayVis2Str(arrayVis);
-    expect(result).toEqual("");
-  });
-  
-  test("should convert an arrayVis object with one element to a string", () => {
-    const arrayVis = {
-      size: () => 1,
-      get: (index) => ({ value: "single" }),
-    };
-    const result = arrayVis2Str(arrayVis);
-    expect(result).toEqual("single");
-  });
-  
+})  
   
